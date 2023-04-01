351bd8b7a5836da3020ac05768a418c6e3c067e0
taiki-umetsu <taiki.umetsu.st@gmail.com>
2023-04-01 22:18:20 +0900

    Skill page

---
+++ src/components/Skill.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  Show,
+  SimpleShowLayout,
+  Edit,
+  SimpleForm,
+  TextInput,
+  Create,
+  DateField,
+  SelectInput,
+} from "react-admin";
+
+const LEVEL_DESCRIPTION = {
+  0: "beginner",
+  10: "professional",
+};
+
+const levelChoices = Object.entries(LEVEL_DESCRIPTION).map(([key, value]) => ({
+  id: key,
+  name: value,
+}));
+
+export const SkillList = (props: any) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="title" />
+      <TextField source="level_description" />
+      <DateField source="created_at" />
+      <DateField source="updated_at" />
+    </Datagrid>
+  </List>
+);
+
+export const SkillShow = (props: any) => (
+  <Show {...props}>
+    <SimpleShowLayout>
+      <TextField source="title" />
+      <TextField source="level_description" />
+      <DateField source="created_at" showTime />
+      <DateField source="updated_at" showTime />
+    </SimpleShowLayout>
+  </Show>
+);
+
+export const SkillEdit = (props: any) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="title" />
+      <SelectInput source="level" choices={levelChoices} />
+    </SimpleForm>
+  </Edit>
+);
+
+export const SkillCreate = (props: any) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="title" />
+      <SelectInput source="level" choices={levelChoices} />
+    </SimpleForm>
+  </Create>
+);
